52e410e343a8b2021bd0e3cd744677f5d9dd4b37
nilwes <58735688+nilwes@users.noreply.github.com>
2021-10-28 19:53:17 +0200

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "wintermute";
+$dbname = "THP-Database";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully<br>";
+
+$temp  = mysqli_real_escape_string($conn, $_GET['Temp']);
+$hum   = mysqli_real_escape_string($conn, $_GET['Hum']);
+$press = mysqli_real_escape_string($conn, $_GET['Press']);
+
+$sql = mysqli_query($conn, "INSERT INTO THPData (Temp, Hum, Press) VALUES (" . $temp . "," . $hum$
+
+mysqli_close($conn);
+?>
